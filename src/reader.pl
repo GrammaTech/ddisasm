dc259fcbf91779d39a920b88b921d1a09d6b4628
Antonio Flores Montoya <afloresmontoya@grammatech.com>
2018-02-05 18:32:20 -0500

    Experimenting with overlaping regions

--- src/reader.pl
+++ src/reader.pl
@@ -19,18 +19,23 @@ read_binary([File|_Args]):-
    
 decode_sections(File,Dir):-
 	Sections=[
-		'.eh_frame',
+	%	'.eh_frame',
 		'.text',
-		'.plt',
-		'.plt.got',
+	%	'.plt',
+	%	'.plt.got',
 		'.init',
 		'.fini'],
-	 Data_sections=['.data','.rodata'],
+	Data_sections=[
+	    '.got',
+	   % '.got.plt',
+	    '.data',
+	    '.rodata'],
 	 foldl(collect_section_args(' --sect '),Sections,[],Sect_args),
 	 foldl(collect_section_args(' --data_sect '),Data_sections,[],Data_sect_args),
 	 atomic_list_concat(Sect_args,Section_chain),
 	 atomic_list_concat(Data_sect_args,Data_section_chain),
-	 atomic_list_concat(['./souffle_disasm ',' --file ',File,' --dir ',Dir,'/',Section_chain,Data_section_chain],Cmd),
+	 atomic_list_concat(['./souffle_disasm ',' --file ',File,
+			     ' --dir ',Dir,'/',Section_chain,Data_section_chain],Cmd),
 	 format('cmd: ~p~n',[Cmd]),
 	 shell(Cmd).
    
@@ -45,27 +50,46 @@ call_souffle(Dir):-
 	
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 result_descriptors([
-	res(valid4sure,2,'.csv'),
-	res(maybe_valid2,1,'.csv'),
-	res(function_symbol,2,'.csv'),
 	res(section,3,'.facts'),
-	res(block_start,1,'.csv'),
 	res(instruction,6,'.facts'),
 	res(op_regdirect,2,'.facts'),
 	res(op_immediate,2,'.facts'),
-	res(op_indirect,7,'.facts')
-	]).
+	res(op_indirect,7,'.facts'),
 
-:-dynamic maybe_valid2/1.
-:-dynamic valid4sure/2.
-:-dynamic function_symbol/2.
-:-dynamic block_start/1.
+	res(direct_jump,2,'.csv'),	
+	res(reg_computed_jump,1,'.csv'),
+	res(indirect_computed_jump,1,'.csv'),
+	res(pc_relative_jump,2,'.csv'),
+	res(direct_call,2,'.csv'),
+
+
+	res(likely_ea2,2,'.csv'),
+	res(possible_ea2,1,'.csv'),
+	res(function_symbol,2,'.csv'),
+	res(block_start,1,'.csv'),
+	res(conflict3,2,'.csv'),
+
+	res(likely_ea3,2,'.csv')
+	]).
 
 :-dynamic section/3.
 :-dynamic instruction/6.
 :-dynamic op_regdirect/2.
 :-dynamic op_immediate/2.
 :-dynamic op_indirect/7.
+
+:-dynamic direct_jump/2.
+:-dynamic reg_computed_jump/1.
+:-dynamic indirect_computed_jump/1.
+:-dynamic pc_relative_jump/2.
+
+:-dynamic likely_ea2/2.
+:-dynamic possible_ea2/1.
+:-dynamic function_symbol/2.
+:-dynamic block_start/1.
+:-dynamic conflict3/2.
+
+:-dynamic likely_ea3/2.
 
 collect_results(Dir,results(Results)):-
 	result_descriptors(Descriptors),
@@ -104,32 +128,40 @@ pp_instruction(instruction(EA,_Size,OpCode,Op1,Op2,Op3)):-
 		format('Section ~p:~n',[Section_name])
 		;
 		true),
-	maybe_valid2(EA),!,
+	(likely_ea3(EA,_)
+         %i'm feeling lucky
+	 %;possible_ea2(EA)
+	),!,
 	exclude(\Op^(Op=none),[Op1,Op2,Op3],Ops),
 	maplist(pp_op,Ops,Pretty_ops),
-	convlist(get_comment,Ops,Comments),
+	%useful info
+	convlist(get_comment,Ops,Op_comments),
+	get_ea_comments(EA,Comments),
+	append(Comments,Op_comments,All_comments),
 	reverse(Pretty_ops,Pretty_ops_rev),
 
 	(
-		function_symbol(EA,Name),
-		format('Function ~p:~n',[Name])
-	;
-	 	block_start(EA),
-	  	format('  Label ~16R:~n',[EA]) 
-	;
-		true
-	),!,
-	
-	(valid4sure(EA,_Parent)->
-		format('         ~16R:   ~p',[EA,OpCode])
-		;
-		format('~p        ~16R:   ~p',['?',EA,OpCode])
-	),
+	    (
+		(function_symbol(EA,Name),Maybe='')
+	     ;
+	     (member(is_called,Comments),Name='unknown',Maybe='(maybe)')
+	    ),
+	    
+	    format(';----------------------------------- ~n',[]),
+	    format('   Function~p ~p:~n',[Maybe,Name]),
+	    format(';----------------------------------- ~n',[])
+	 ;
+	 block_start(EA),
+	 format('  Label ~16R:~n',[EA]) 
+	 ;
+	 true
+	),!,	
+	format('         ~16R:   ~p',[EA,OpCode]), 
 	maplist(print_with_space,Pretty_ops_rev),
 	% print the names of the immediates if they are functions
-	(Comments\=[]->
+	(All_comments\=[]->
 		format('          # ',[]),
-		maplist(print_with_space,Comments)
+		maplist(print_with_space,All_comments)
 		;true
 	),
 	nl.
@@ -137,17 +169,39 @@ pp_instruction(instruction(EA,_Size,OpCode,Op1,Op2,Op3)):-
 pp_instruction(_).
 
 
-print_with_space(Op+Offset):-!,
-	format(' [~p+~16R] ',[Op,Offset]). 
-print_with_space([Op+Offset]):-!,
-	format(' [~p+~16R] ',[Op,Offset]). 
-
 print_with_space(Op):-
-	number(Op),!,
-	format(' ~16R ',[Op]). 
-print_with_space(Op):-
-	format(' ~p ',Op). 
+	format(' ~p ',[Op]). 
 		
+
+get_ea_comments(EA,Comments):-
+    setof(Comment,
+	    ea_comment(EA,Comment),
+	    Comments),!.
+get_ea_comments(_EA,[]).
+
+ea_comment(EA,is_called):-
+    direct_call(_,EA).
+
+ea_comment(EA,conflict_with(EA2)):-
+    conflict3(EA2,EA).
+ea_comment(EA,conflict_with(EA2)):-
+    conflict3(EA,EA2).
+
+ea_comment(EA,not_in_block):-
+    \+likely_ea3(EA,_).
+
+ea_comment(EA,is_jumped_to_from(Str_or)):-
+    direct_jump(Or,EA),
+    format(string(Str_or),'~16R',[Or]).
+
+ea_comment(EA,reg_jump):-
+    reg_computed_jump(EA).
+ea_comment(EA,indirect_jump):-
+    indirect_computed_jump(EA).
+
+ea_comment(EA,pc_relative_jump(Dest)):-
+    pc_relative_jump(EA,Dest).
+
 get_comment(Op,Name):-
 	Op=immediate(Num),
 	Num\=0,
@@ -156,12 +210,22 @@ get_comment(Op,Name):-
 
 	
 pp_op(reg(Name),Name).
-pp_op(immediate(Num),Num).
+pp_op(immediate(Num),Num_hex):-
+      format(string(Num_hex),'~16R',[Num]).
 
 pp_op(indirect('NullSReg',Reg,'NullReg64',1,0,_),[Reg]). 	 
-pp_op(indirect('NullSReg',Reg,'NullReg64',1,Offset,_),[Reg+Offset]).
+pp_op(indirect('NullSReg',Reg,'NullReg64',1,Offset,_),[Reg+Offset_hex]):-
+       format(string(Offset_hex),'~16R',[Offset]).
 
-%pp_op(indirect(nullsreg,Reg,nullreg64,1,Offset,_)),[Reg+Offset]).
+pp_op(indirect('NullSReg','NullReg64',Reg_index,Mult,Offset,_),[Offset_hex+Reg_index*Mult]):-
+    format(string(Offset_hex),'~16R',[Offset]).
+pp_op(indirect('NullSReg',Reg,Reg_index,Mult,0,_),[Reg+Reg_index*Mult]).
+pp_op(indirect('NullSReg',Reg,Reg_index,Mult,Offset,_),[Reg+Offset_hex+Reg_index*Mult]):-
+       format(string(Offset_hex),'~16R',[Offset]).
+
+
+pp_op(indirect(SReg,'NullReg64','NullReg64',1,Offset,_),[SReg:Offset_hex]):-
+       format(string(Offset_hex),'~16R',[Offset]).
 
 
 pp_op(Else,Else).	 
