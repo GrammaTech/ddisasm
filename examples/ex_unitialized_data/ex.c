c28911101595de945945d9530f6fedca0b82488c
Antonio Flores Montoya <afloresmontoya@grammatech.com>
2018-02-15 12:00:54 -0500

    2 more examples with floats and uninitialized data

---
+++ examples/ex_unitialized_data/ex.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int array[1000];
+
+void init(){
+    for(int i=0;i<10;++i)
+        array[i]=i;
+}
+
+void print(){
+    for(int i=0;i<10;++i)
+        printf("%i\n",array[i]);
+}
+
+int main() {
+    puts("Storing data"); 
+    init();
+    puts("Printing data");
+    print();
+    return 0;
+}
+
