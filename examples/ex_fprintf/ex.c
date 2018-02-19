7701008126ff0282abfe67ad1e00f2ee256243de
Antonio Flores Montoya <afloresmontoya@grammatech.com>
2018-02-19 17:39:36 -0500

    new examples:  -printing to stderr  -iterating over struct with optimizations  -calling stat

---
+++ examples/ex_fprintf/ex.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main() {
+    fprintf(stderr,"%s %s %s\n", "a", " string", " in stderr");
+    fprintf(stdout,"%s %s %s\n", "a", " string", " in stdout");
+	return 0;
+}
+
