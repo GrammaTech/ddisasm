cb712b3c1213b9be0ba683d83d93e9ac420383a2
Antonio Flores Montoya <afloresmontoya@grammatech.com>
2018-06-14 15:32:06 -0400

    recompile with -pie or -no-pie according to the original binary

--- bin/reassemble_and_test.sh
+++ bin/reassemble_and_test.sh
@@ -59,11 +59,14 @@ if [ $strip == 1 ]; then
     strip --strip-unneeded "$dir/$exe"
 fi
 
+
 printf "# Disassembling $exe into $exe.s with flags: $stir\n"
 if !(time(./disasm "$dir/$exe" -asm $stir > "$dir/$exe.s") 2>/tmp/time.txt); then
     printf "# ${red}Disassembly failed${normal}\n"
     exit 1
 fi
+
+
 decode_time=$(cat /tmp/time.txt | grep -m 1 seconds)
 dl_time=$(cat /tmp/time.txt | grep -m 2 seconds | tail -n1)
 decode_time=${decode_time#*in }
@@ -78,9 +81,19 @@ printf "#Stats: Time $time Decode $decode_time Datalog $dl_time Size $size\n"
 printf "  OK\n"
 printf "Copying old binary to $dir/$exe.old\n"
 cp $dir/$exe $dir/$exe.old
+binary_type=$(cat $dir/dl_files/binary_type.facts)
+printf "# Binary of type $binary_type\n"
+
+pie_flag=""
+if [[  $binary_type == "DYN" ]]; then
+    pie_flag="-pie"
+else
+    pie_flag="-no-pie"
+fi
+    
 printf "# Reassembling...\n"
 
-if !($compiler "$dir/$exe.s" $@  -o  "$dir/$exe"); then
+if !($compiler "$dir/$exe.s" $pie_flag $@  -o  "$dir/$exe"); then
     printf "# ${red}Reassembly failed ${normal}\n"
     exit 1
 fi
