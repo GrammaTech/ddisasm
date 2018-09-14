28f20d15fdf492376ae4b1ec804fb8a40da54fa7
Eric Schulte <eschulte@grammatech.com>
2018-09-14 11:17:17 -0400

    License in headers

--- src/Elf_reader.h
+++ src/Elf_reader.h
@@ -1,3 +1,17 @@
+//===- Elf_reader.h ---------------------------------------------*- C++ -*-===//
+//
+//  Copyright (C) 2018 GrammaTech, Inc.
+//
+//  This code is licensed under the GPL V3 license. See the LICENSE file in the
+//  project root for license terms.
+//
+//  This project is sponsored by the Office of Naval Research, One Liberty
+//  Center, 875 N. Randolph Street, Arlington, VA 22203 under contract #
+//  N68335-17-C-0700.  The content of the information does not necessarily
+//  reflect the position or policy of the Government and no official
+//  endorsement should be inferred.
+//
+//===----------------------------------------------------------------------===//
 /*
  * Elf_reader.h
  *
