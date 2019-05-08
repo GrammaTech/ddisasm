ac144b8c2ea759d063658d2616452b64b6c9e47a
Antonio Flores Montoya <afloresmontoya@grammatech.com>
2019-05-08 17:43:45 -0400

    making ex_pointerReatribution2 challenging for real

--- examples/ex_pointerReatribution2/ex.c
+++ examples/ex_pointerReatribution2/ex.c
@@ -4,44 +4,29 @@
 
 int
 counters[2] = {0};
+
+void print(){
+    for(int i=0;i<2;++i)
+        printf("%i\n",counters[i]);
+}
+
 int
 main()
 {
     int  input;
     input = getchar();
-    switch  (input - 'A')
+    switch  (input)
     {
-    case  0:
+    case  'A':
         puts("option A");
         break;
-    case  1:
+    case  'B':
         puts("option B");
         break;
-    case  2:
-        puts("option C");
-        break;
-    case  3:
-        puts("option D");
-        break;
-    case  4:
-        puts("option E");
-        break;
-    case  5:
-        puts("option F");
-        break;
-    case  6:
-        puts("option G");
-        break;
-    case  7:
-        puts("option H");
-        break;
-    case  8:
-        puts("option I");
-        break;
-        
     default:
         puts("Unknown option.");
         return 0;
     }
     counters[input - 'A'] ++;
+    print();
 }
