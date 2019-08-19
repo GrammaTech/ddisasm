bf198d5a631d12d6b143789e7afec47b5d0c0812
Antonio Flores Montoya <afloresmontoya@grammatech.com>
2019-08-19 09:36:06 -0400

    rename intermediate clause in isNonZeroDataSection

--- src/Elf_reader.cpp
+++ src/Elf_reader.cpp
@@ -264,9 +264,9 @@ vector<Section> Elf_reader::get_non_zero_data_sections()
     auto isNonZeroDataSection = [](Section& s) {
         bool is_allocated = s.flags & SHF_ALLOC;
         bool is_not_executable = !(s.flags & SHF_EXECINSTR);
-        bool is_program_data = s.type == SHT_PROGBITS || s.type == SHT_INIT_ARRAY
-                            || s.type == SHT_FINI_ARRAY || s.type == SHT_PREINIT_ARRAY;
-        return is_allocated && is_not_executable && is_program_data;
+        bool is_non_zero_program_data = s.type == SHT_PROGBITS || s.type == SHT_INIT_ARRAY
+                                        || s.type == SHT_FINI_ARRAY || s.type == SHT_PREINIT_ARRAY;
+        return is_allocated && is_not_executable && is_non_zero_program_data;
     };
     sections.erase(remove_if(begin(sections), end(sections), not_fn(isNonZeroDataSection)),
                    end(sections));
