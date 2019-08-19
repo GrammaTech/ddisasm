cef3ad39ed807d9618682093f19d821309ec4f41
Antonio Flores Montoya <afloresmontoya@grammatech.com>
2019-08-19 10:54:30 -0400

    simplify small tests (remove test.sh script)

--- tests/test_small.sh
+++ tests/test_small.sh
@@ -54,6 +54,7 @@ for ((i = 0; i < ${#examples[@]}; i++)); do
 	export CXX=${cpp_compilers[$j]}
 	for optimization in  "${optimizations[@]}"; do
 	    export CFLAGS="$optimization $EXTRA_FLAGS"
+	    export CXXFLAGS="$optimization $EXTRA_FLAGS"
 	    echo "#Example $file with $compiler $CFLAGS"
         if !(./reassemble_and_test.sh $strip -stir $dir${examples[$i]}) then
 	       ((error++))
