4e30a811b86b26b22c2d295d5a08ff5d89dcd695
aeflores <afloresmontoya@grammatech.com>
2019-09-24 18:58:38 -0400

    test function inference for clang-compiled programs

--- tests/function_inference_test.py
+++ tests/function_inference_test.py
@@ -48,6 +48,27 @@ class TestFunctionInference(unittest.TestCase):
     def test_functions_uninitialized_data(self): self.check_function_inference(ex_dir/'ex_uninitialized_data','ex','gcc','g++','-O3')
     def test_functions_virtualDispatch(self): self.check_function_inference(ex_dir/'ex_virtualDispatch','ex','gcc','g++','-O3')
 
+    def test_functions_ex1_clang(self): self.check_function_inference(ex_dir/'ex1','ex','clang','clang++','-O3')
+    def test_functions_2modulesPIC_clang(self): self.check_function_inference(ex_dir/'ex_2modulesPIC','ex','clang','clang++','-O3')
+    def test_functions_confusing_data_clang(self): self.check_function_inference(ex_dir/'ex1','ex','clang','clang++','-O3')
+    def test_functions_exceptions1_clang(self): self.check_function_inference(ex_dir/'ex_exceptions1','ex','clang','clang++','-O3')
+    def test_functions_exceptions2_clang(self): self.check_function_inference(ex_dir/'ex_exceptions2','ex','clang','clang++','-O3')
+    def test_functions_exceptions3_clang(self): self.check_function_inference(ex_dir/'ex_exceptions3','ex','clang','clang++','-O3')
+    def test_functions_false_pointer_array_clang(self): self.check_function_inference(ex_dir/'ex_false_pointer_array','ex','clang','clang++','-O3')
+    def test_functions_float_clang(self): self.check_function_inference(ex_dir/'ex_float','ex','clang','clang++','-O3')
+    def test_functions_fprintf_clang(self): self.check_function_inference(ex_dir/'ex_fprintf','ex','clang','clang++','-O3')
+    def test_functions_getoptlong_clang(self): self.check_function_inference(ex_dir/'ex_getoptlong','ex','clang','clang++','-O3')
+    def test_functions_memberPointer_clang(self): self.check_function_inference(ex_dir/'ex_memberPointer','ex','clang','clang++','-O3')
+    def test_functions_noreturn_clang(self): self.check_function_inference(ex_dir/'ex_noreturn','ex','clang','clang++','-O3')
+    def test_functions_pointerReatribution_clang(self): self.check_function_inference(ex_dir/'ex_pointerReatribution','ex','clang','clang++','-O3')
+    def test_functions_pointerReatribution2_clang(self): self.check_function_inference(ex_dir/'ex_pointerReatribution2','ex','clang','clang++','-O3')
+    def test_functions_pointerReatribution3_clang(self): self.check_function_inference(ex_dir/'ex_pointerReatribution3','ex','clang','clang++','-O3')
+    def test_functions_stat_clang(self): self.check_function_inference(ex_dir/'ex_stat','ex','clang','clang++','-O3')
+    def test_functions_struct_clang(self): self.check_function_inference(ex_dir/'ex_struct','ex','clang','clang++','-O3')
+    def test_functions_switch_clang(self): self.check_function_inference(ex_dir/'ex_switch','ex','clang','clang++','-O3')
+    def test_functions_uninitialized_data_clang(self): self.check_function_inference(ex_dir/'ex_uninitialized_data','ex','clang','clang++','-O3')
+    def test_functions_virtualDispatch_clang(self): self.check_function_inference(ex_dir/'ex_virtualDispatch','ex','clang','clang++','-O3')
+
 
 if __name__ == '__main__':
     unittest.main()
