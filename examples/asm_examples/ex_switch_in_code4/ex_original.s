3300bf0cca1e19a9535c985fad23cd52f256dbdd
Kevin Warrick <kwarrick@grammatech.com>
2020-01-22 11:07:42 -0500

    Simplify test

--- examples/asm_examples/ex_switch_in_code4/ex_original.s
+++ examples/asm_examples/ex_switch_in_code4/ex_original.s
@@ -25,35 +25,12 @@ switch:
 
 .L33:
 .long   .L32-.L33
-.long   .L34-.L33
-.long   .L35-.L33
-.long   .L35-.L33
-.long   .L36-.L33
 
     .text
 .L32:
   lea	rdi, [rip + .L.str]
   call	puts@PLT
 
-  mov eax, 1
-  jmp switch
-.L43:
-  mov ebx, 43
-  mov eax, 1
-  int 0x80
-.L34:
-  lea	rdi, [rip + .L.str.1]
-  call	puts@PLT
-
-  mov eax, 2
-  jmp switch
-.L35:
-  lea	rdi, [rip + .L.str.5]
-  call	puts@PLT
-
-  mov eax, 4
-  jmp switch
-.L36:
   mov ebx, 0
   mov eax, 1
   int 0x80
@@ -63,28 +40,3 @@ switch:
   .L.str:
 .asciz	"one"
 .size	.L.str, 4
-
-.type	.L.str.1,@object        # @.str.1
-  .L.str.1:
-.asciz	"two"
-.size	.L.str.1, 4
-
-.type	.L.str.2,@object        # @.str.2
-  .L.str.2:
-.asciz	"three"
-.size	.L.str.2, 6
-
-.type	.L.str.3,@object        # @.str.3
-  .L.str.3:
-.asciz	"four"
-.size	.L.str.3, 5
-
-.type	.L.str.4,@object        # @.str.4
-  .L.str.4:
-.asciz	"last"
-.size	.L.str.4, 5
-
-.type	.L.str.5,@object        # @.str.5
-  .L.str.5:
-.asciz	"!!!Hello World!!!"
-.size	.L.str.5, 18
