f06df693f0b1fd264e9988d8a6426090bab76277
Brian Fairservice <bfairservice@grammatech.com>
2020-01-29 09:00:35 -0500

    Install gtirb during windows build

--- .ci/build-windows.sh
+++ .ci/build-windows.sh
@@ -7,7 +7,7 @@ BUILD_TYPE=$1
 mkdir gtirb/build
 pushd gtirb/build
 cmd.exe /C "C:\\VS\\VC\\Auxiliary\\Build\\vcvars64.bat && C:\\PROGRA~1\\CMake\\bin\\cmake.exe -G "Ninja" -DBOOST_ROOT=\"C:\\Boost\" -DCMAKE_PREFIX_PATH=\"C:\\Program Files (x86)\\protobuf\" -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DPYTHON=C:\\Python38\\python.exe -DCMAKE_INSTALL_PREFIX=\"C:\\gtirb-$BUILD_TYPE\" .."
-cmd.exe /C "C:\\VS\\VC\\Auxiliary\\Build\\vcvars64.bat && ninja"
+cmd.exe /C "C:\\VS\\VC\\Auxiliary\\Build\\vcvars64.bat && ninja && ninja install"
 PATH="$PATH:$(pwd)/bin"
 popd
 
