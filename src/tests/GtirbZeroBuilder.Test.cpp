0d45a8bbb9c602794967b4604f8b98a0a39b0ac5
Kevin Warrick <kwarrick@grammatech.com>
2020-02-11 17:14:24 -0500

    Add unit tests
    Started unit tests for GtirbZeroBuilder and LIEFBinaryReader

---
+++ src/tests/GtirbZeroBuilder.Test.cpp
@@ -0,0 +1,40 @@
+#include <gtest/gtest.h>
+
+#include <LIEF/LIEF.hpp>
+#include <gtirb/gtirb.hpp>
+
+#include "../GtirbZeroBuilder.h"
+
+class GtirbZeroBuilderTest : public ::testing::TestWithParam<const char*>
+{
+};
+
+TEST_P(GtirbZeroBuilderTest, buildSections)
+{
+    gtirb::Context Context;
+    const std::string& Path(GetParam());
+    gtirb::IR* IR = buildZeroIR(Path, Context);
+    gtirb::Module& Module = *(IR->modules().begin());
+
+    std::unique_ptr<LIEF::ELF::Binary> ELF = LIEF::ELF::Parser::parse(Path);
+    std::unordered_set<std::string> Names;
+    std::unordered_map<std::string, size_t> Sizes;
+
+    for(const auto& Section : ELF->sections())
+    {
+        if(Section.has(LIEF::ELF::ELF_SECTION_FLAGS::SHF_ALLOC))
+        {
+            Names.insert(Section.name());
+            Sizes[Section.name()] = Section.size();
+        }
+    }
+
+    for(const auto& Section : Module.sections())
+    {
+        const std::string& Name = Section.getName();
+        EXPECT_EQ(Names.count(Name), 1);
+        EXPECT_EQ(Sizes[Name], Section.getSize());
+    }
+}
+
+INSTANTIATE_TEST_SUITE_P(InstantiationName, GtirbZeroBuilderTest, testing::Values("hello.x64.elf"));
