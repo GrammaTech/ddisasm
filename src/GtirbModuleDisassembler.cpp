122df44b7a77cb1e8aae5da89293b2e8092ae538
Kevin Warrick <kwarrick@grammatech.com>
2020-02-11 21:57:44 -0500

    Fix addBlock

--- src/GtirbModuleDisassembler.cpp
+++ src/GtirbModuleDisassembler.cpp
@@ -822,7 +822,7 @@ void buildDataGroups(gtirb::Context &context, gtirb::Module &module, souffle::So
                     const auto specialType = symbolSpecialTypes.find(currentAddr);
                     if(specialType != symbolSpecialTypes.end())
                         typesTable[d->getUUID()] = specialType->Type;
-                    byteInterval.addBlock(blockOffset, d);
+                    byteInterval.addBlock<gtirb::DataBlock>(blockOffset, d);
                     currentAddr += d->getSize();
                 }
             }
