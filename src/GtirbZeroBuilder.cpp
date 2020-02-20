b4615bdbb263b7ea88f344b6932ec5e3139c8ac4
Kevin Warrick <kwarrick@grammatech.com>
2020-02-20 11:22:38 -0500

    Use effective addresses

--- src/GtirbZeroBuilder.cpp
+++ src/GtirbZeroBuilder.cpp
@@ -114,10 +114,12 @@ void buildSections(gtirb::Module &module, std::shared_ptr<BinaryReader> binary,
             if(auto sectionData = binary->get_section_content_and_address(binSection.name))
             {
                 // Add allocated section contents to a single contiguous ByteInterval.
+                gtirb::Addr sectionAddr =
+                    gtirb::Addr(binary->get_base_address() + binSection.address);
                 std::vector<uint8_t> &sectionBytes = std::get<0>(*sectionData);
-                gtirb::ByteInterval *byteInterval = section->addByteInterval(
-                    context, gtirb::Addr(binSection.address), sectionBytes.begin(),
-                    sectionBytes.end(), binSection.size);
+                gtirb::ByteInterval *byteInterval =
+                    section->addByteInterval(context, sectionAddr, sectionBytes.begin(),
+                                             sectionBytes.end(), binSection.size);
                 // Fill incomplete section with zeroes.
                 if(sectionBytes.size() < binSection.size)
                 {
@@ -184,6 +186,7 @@ void addEntryBlock(gtirb::Module &Module, std::shared_ptr<BinaryReader> Binary,
             Module.setEntryPoint(Block);
         }
     }
+    assert(Module.getEntryPoint() && "Failed to set module entry point.");
 }
 
 void addAuxiliaryTables(gtirb::Module &module, std::shared_ptr<BinaryReader> binary)
