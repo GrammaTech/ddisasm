c3ec503ea5e8491ca51aafd4d6c07a6abddd4c4f
Brian Fairservice <bfairservice@grammatech.com>
2020-04-17 14:34:47 -0400

    Use dockerignore file to omit build dirs

--- .ci/build-linux.sh
+++ .ci/build-linux.sh
@@ -12,6 +12,5 @@ set -o pipefail
 CXX_COMPILER=$1
 
 # Build ddisasm
-rm -rf /ddisasm/build /ddisasm/CMakeCache.txt /ddisasm/CMakeFiles /ddisasm/CMakeScripts
 cd /ddisasm
 cmake ./  -Bbuild -DCMAKE_CXX_COMPILER=${CXX_COMPILER} -DLIEF_ROOT=/usr/ && cd build && make
