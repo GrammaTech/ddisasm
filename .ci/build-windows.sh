69c0084a64ce84dfbf2e566da0f31d3770795f79
Brian Fairservice <bfairservice@grammatech.com>
2020-04-30 13:19:42 -0400

    Fix path to windows tests

--- .ci/build-windows.sh
+++ .ci/build-windows.sh
@@ -64,4 +64,9 @@ cp ./DDISASM-*-win64.zip ../
 
 # Test ddisasm
 cp $BASE_DIRECTORY/bin/*.dll ./bin
-cmd.exe /C "C:\\VS\\VC\\Auxiliary\\Build\\vcvars64.bat && C:\\PROGRA~1\\CMake\\bin\\ctest.exe -V"
+
+# We don't run the tests on the debug build because they take over 3 hours.
+if [ "$BUILD_TYPE" == 'Release' ]; then
+  DDISASM_BIN=$(cygpath -w $(pwd)/bin)
+  PATH="$PATH;$DDISASM_BIN" cmd.exe /C "C:\\VS\\VC\\Auxiliary\\Build\\vcvars64.bat && C:\\PROGRA~1\\CMake\\bin\\ctest.exe -V"
+fi
