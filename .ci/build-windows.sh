ec178d66e5532401bcd592607b377de7d49b5ea7
Brian Fairservice <bfairservice@grammatech.com>
2020-05-08 14:48:33 -0400

    Use correct job name

--- .ci/build-windows.sh
+++ .ci/build-windows.sh
@@ -14,6 +14,7 @@ BUILD_TYPE=$1
 # Install GTIRB
 GTIRB_BRANCH=$(sed -e '/find_package(gtirb /!d' -e 's/[^.0-9]//g' CMakeLists.txt)
 GTIRB_BRANCH=$((grep -Eo "check_gtirb_branch\([^)]+" CMakeLists.txt | sed 's/check_gtirb_branch(//') || echo v${GTIRB_BRANCH:?})
+FIXED_BUILD_TYPE=$(echo $BUILD_TYPE | sed 's/Debug/Debug/;s/RelWithDebInfo/Release/')
 curl -L https://git.grammatech.com/rewriting/gtirb/-/jobs/artifacts/${GTIRB_BRANCH}/download?job=build-windows-msvc-${BUILD_TYPE,,} --output "gtirb-artifacts.zip"
 unzip gtirb-artifacts.zip
 unzip GTIRB-*-win64.zip
@@ -44,7 +45,7 @@ GTIRB_DIR=$(cygpath -m $(realpath $(find ./ -type d -name GTIRB-*-win64)/lib/gti
 GTIRB_PPRINTER_DIR=$(cygpath -m $(realpath $(find ./ -type d -name gtirb_pprinter-*-win64)/lib/gtirb_pprinter))
 mkdir -p build
 cd build
-cmd.exe /C "C:\\VS\\VC\\Auxiliary\\Build\\vcvars64.bat && C:\\PROGRA~1\\CMake\\bin\\cmake.exe -G \"Ninja\" -DBOOST_ROOT=\"C:\\Boost\" -DCAPSTONE=\"C:\\capstone-${BUILD_TYPE}\\lib\\capstone.lib\" -DLIEF_ROOT=\"C:\\lief-${BUILD_TYPE}\" -DDDISASM_USE_SYSTEM_BOOST=ON -DDDISASM_BUILD_SHARED_LIBS=ON -DCMAKE_CXX_FLAGS=\"/I C:\\capstone-${BUILD_TYPE}\\include /I C:\\users\\vagrant\\AppData\\local\\Packages\\CanonicalGroupLimited.Ubuntu18.04onWindows_79rhkp1fndgsc\\localstate\\rootfs\\usr\\local\\include /DBOOST_ALL_DYN_LINK\" -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_PROGRAM_PATH=\"C:\\capstone-${BUILD_TYPE}\\bin\" -DPYTHON=C:\\Python38\\python.exe  -Dgtirb_DIR=$GTIRB_DIR -Dgtirb_pprinter_DIR=$GTIRB_PPRINTER_DIR .."
+cmd.exe /C "C:\\VS\\VC\\Auxiliary\\Build\\vcvars64.bat && C:\\PROGRA~1\\CMake\\bin\\cmake.exe -G \"Ninja\" -DBOOST_ROOT=\"C:\\Boost\" -DCAPSTONE=\"C:\\capstone-${FIXED_BUILD_TYPE}\\lib\\capstone.lib\" -DLIEF_ROOT=\"C:\\lief-${FIXED_BUILD_TYPE}\" -DDDISASM_USE_SYSTEM_BOOST=ON -DDDISASM_BUILD_SHARED_LIBS=ON -DCMAKE_CXX_FLAGS=\"/I C:\\capstone-${FIXED_BUILD_TYPE}\\include /I C:\\users\\vagrant\\AppData\\local\\Packages\\CanonicalGroupLimited.Ubuntu18.04onWindows_79rhkp1fndgsc\\localstate\\rootfs\\usr\\local\\include /DBOOST_ALL_DYN_LINK\" -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_PROGRAM_PATH=\"C:\\capstone-${FIXED_BUILD_TYPE}\\bin\" -DPYTHON=C:\\Python38\\python.exe  -Dgtirb_DIR=$GTIRB_DIR -Dgtirb_pprinter_DIR=$GTIRB_PPRINTER_DIR .."
 cmd.exe /C "C:\\VS\\VC\\Auxiliary\\Build\\vcvars64.bat && ninja"
 
 # Generate windows package
@@ -60,7 +61,7 @@ cp $GTIRB_DLL $BASE_DIRECTORY/bin/$(basename $GTIRB_DLL)
 GTIRB_PPRINTER_DLL=$GTIRB_PPRINTER_DIR/../../bin/gtirb_pprinter$(echo $BUILD_TYPE | sed 's/Debug/d/;s/RelWithDebInfo//').dll
 cp $GTIRB_PPRINTER_DLL $BASE_DIRECTORY/bin/$(basename $GTIRB_PPRINTER_DLL)
 cp bin/ddisasm.pdb $BASE_DIRECTORY/bin
-cp ./$BASE_DIRECTORY ../
+cp -r ./$BASE_DIRECTORY ../
 
 # Test ddisasm
 cp $BASE_DIRECTORY/bin/*.dll ./bin
