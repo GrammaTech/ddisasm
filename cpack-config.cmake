8296ef087a4a73948a45b50a77c32c6251903523
Antonio Flores Montoya <afloresmontoya@grammatech.com>
2020-07-06 12:42:07 -0400

    fix capstone version.

--- cpack-config.cmake
+++ cpack-config.cmake
@@ -6,7 +6,7 @@ if("${CPACK_DDISASM_DEBIAN_PACKAGE}" STREQUAL "ddisasm")
   set(CPACK_PACKAGE_FILE_NAME "ddisasm")
   set(CPACK_COMPONENTS_ALL ddisasm)
   set(CPACK_DEBIAN_PACKAGE_DEPENDS
-      "libstdc++6, libc6, libgcc1, libgomp1, libgtirb (>=${CPACK_GTIRB_VERSION}), libgtirb-pprinter (>=${CPACK_GTIRB_PPRINTER_VERSION}), libboost (>=1.67) | libboost1.67, libcapstone-dev (=1:4.0.1-gt1)"
+      "libstdc++6, libc6, libgcc1, libgomp1, libgtirb (>=${CPACK_GTIRB_VERSION}), libgtirb-pprinter (>=${CPACK_GTIRB_PPRINTER_VERSION}), libboost (>=1.67) | libboost1.67, libcapstone-dev (=1:4.0.1-gt2)"
   )
 elseif("${CPACK_DDISASM_DEBIAN_PACKAGE}" STREQUAL "debug")
   set(CPACK_DEBIAN_PACKAGE_NAME "ddisasm-dbg")
