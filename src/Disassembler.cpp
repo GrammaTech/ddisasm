308f8fe75a9ec48de02e72f845de2f2a6dda8d29
Kevin Warrick <kwarrick@grammatech.com>
2020-11-18 21:23:18 -0500

    Remove unused relation

--- src/Disassembler.cpp
+++ src/Disassembler.cpp
@@ -785,7 +785,6 @@ void buildDataBlocks(gtirb::Context &context, gtirb::Module &module, souffle::So
         convertSortedRelation<VectorByEA<SymbolicExpr>>("symbolic_expr_from_relocation", prog);
     auto symbolMinusSymbol =
         convertSortedRelation<VectorByEA<SymbolMinusSymbol>>("symbol_minus_symbol", prog);
-    // TODO: Use the `symbolic_data_attribute' relation.
 
     auto dataStrings = convertSortedRelation<VectorByEA<StringDataObject>>("string", prog);
     auto symbolSpecialTypes =
