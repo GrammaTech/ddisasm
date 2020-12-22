333cc4b346618e732ed1ef82776346635c07a988
Kevin Warrick <kwarrick@grammatech.com>
2020-12-22 01:40:02 -0500

    Register AuxData type

--- src/tests/Main.Test.cpp
+++ src/tests/Main.Test.cpp
@@ -13,6 +13,7 @@ void registerTestAuxDataTypes()
     gtirb::AuxDataContainer::registerAuxDataType<Padding>();
     gtirb::AuxDataContainer::registerAuxDataType<SymbolForwarding>();
     gtirb::AuxDataContainer::registerAuxDataType<ElfSymbolInfoAD>();
+    gtirb::AuxDataContainer::registerAuxDataType<ElfSymbolTabIdxInfoAD>();
     gtirb::AuxDataContainer::registerAuxDataType<BinaryType>();
     gtirb::AuxDataContainer::registerAuxDataType<Sccs>();
     gtirb::AuxDataContainer::registerAuxDataType<Relocations>();
