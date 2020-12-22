030d0dc540634b8113ce075b89ae57df5a38fd78
Kevin Warrick <kwarrick@grammatech.com>
2020-12-21 23:23:35 -0500

    Register AuxData type

--- src/Registration.cpp
+++ src/Registration.cpp
@@ -42,6 +42,7 @@ void registerAuxDataTypes()
     gtirb::AuxDataContainer::registerAuxDataType<Padding>();
     gtirb::AuxDataContainer::registerAuxDataType<SymbolForwarding>();
     gtirb::AuxDataContainer::registerAuxDataType<ElfSymbolInfoAD>();
+    gtirb::AuxDataContainer::registerAuxDataType<ElfSymbolTabIdxInfoAD>();
     gtirb::AuxDataContainer::registerAuxDataType<DynamicEntries>();
     gtirb::AuxDataContainer::registerAuxDataType<BinaryType>();
     gtirb::AuxDataContainer::registerAuxDataType<Sccs>();
