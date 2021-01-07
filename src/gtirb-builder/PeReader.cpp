9c2be941aa30dac824b72e5d5078534af55c625f
Kevin Warrick <kwarrick@grammatech.com>
2021-01-07 15:15:02 -0500

    Populate binaryType for PE

--- src/gtirb-builder/PeReader.cpp
+++ src/gtirb-builder/PeReader.cpp
@@ -143,7 +143,8 @@ void PeReader::addEntryBlock()
 void PeReader::addAuxData()
 {
     // Add `binaryType' aux data table.
-    std::vector<std::string> BinaryType = {"EXEC"};
+    bool DLL = Pe->header().has_characteristic(LIEF::PE::HEADER_CHARACTERISTICS::IMAGE_FILE_DLL);
+    std::vector<std::string> BinaryType = {Pe->is_pie() ? "DYN" : "EXEC", DLL ? "DLL" : "EXE"};
     Module->addAuxData<gtirb::schema::BinaryType>(std::move(BinaryType));
 
     // Add `libraries' aux data table.
