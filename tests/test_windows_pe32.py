882a5a6c55da13f52e603c3c3860b39d108c1d25
Kevin Warrick <kwarrick@grammatech.com>
2021-01-15 02:01:44 -0500

    Test against different optimization levels

--- tests/test_windows_pe32.py
+++ tests/test_windows_pe32.py
@@ -15,11 +15,11 @@ examples_path = tests_path.parent / "examples"
 
 DISABLED = True
 
-def compile(*sources):
+def compile(*args):
     proc = subprocess.run([
         "cl32",
         "/nologo",
-        *sources
+        *args
         ], stdout=subprocess.DEVNULL)
     return proc.returncode == 0
 
@@ -28,10 +28,10 @@ def rename_symbols(path):
         with open(tests_path / "kernel32.lib") as lib:
             rename_externs(asm, [lib])
 
-def reassemble(*paths):
+def reassemble(*args):
     proc = subprocess.run([
         "ml", "/nologo",
-        *paths,
+        *args,
         "/Fe", "out.exe",
         "/link", "/subsystem:console", "/entry:__EntryPoint"
     ], stdout=subprocess.DEVNULL)
@@ -55,13 +55,18 @@ class TestPE32(unittest.TestCase):
 
     def bdrt(self, name, *sources):
         """Build,Disassemble,Reassemble,Test"""
-        with cd(examples_path / name):
-            self.assertTrue(compile(*sources))
-            self.assertTrue(disassemble("ex.exe", False, extension="asm", extra_args=["-F"]))
-            rename_symbols("ex.exe.asm")
-            self.assertTrue(reassemble("ex.exe.asm"))
-            self.assertTrue(diff_output(["ex.exe"], ["out.exe"]))
-            cleanup()
+
+        def inner(*flags):
+            with cd(examples_path / name):
+                self.assertTrue(compile(*flags, *sources))
+                self.assertTrue(disassemble("ex.exe", False, extension="asm", extra_args=["-F"]))
+                rename_symbols("ex.exe.asm")
+                self.assertTrue(reassemble("ex.exe.asm"))
+                self.assertTrue(diff_output(["ex.exe"], ["out.exe"]))
+                cleanup()
+
+        for flag in ["/Od", "/Ot", "/O1", "/Ox", "/O2"]:
+            inner(flag)
 
     @unittest.skipUnless(not DISABLED, "WIP test cases")
     def test_ex1(self):
@@ -113,54 +118,66 @@ class TestPE32(unittest.TestCase):
 
     @unittest.skipUnless(not DISABLED, "WIP test cases")
     def test_ex_legacy_switch_001(self):
-        with cd(examples_path / "ex_legacy_switch.001"):
-            self.assertTrue(compile("main.c"))
-            self.assertTrue(disassemble("main.exe", False, extension="asm", extra_args=["-F"]))
-            rename_symbols("main.exe.asm")
-            self.assertTrue(reassemble("main.exe.asm"))
-            for char in string.ascii_uppercase:
-                self.assertTrue(diff_output(["main.exe", char], ["out.exe", char]))
-            cleanup()
+        def inner(*flags):
+            with cd(examples_path / "ex_legacy_switch.001"):
+                self.assertTrue(compile(*flags, "main.c"))
+                self.assertTrue(disassemble("main.exe", False, extension="asm", extra_args=["-F"]))
+                rename_symbols("main.exe.asm")
+                self.assertTrue(reassemble("main.exe.asm"))
+                for char in string.ascii_uppercase:
+                    self.assertTrue(diff_output(["main.exe", char], ["out.exe", char]))
+                cleanup()
+        for flag in ["/Od", "/Ot", "/O1", "/Ox", "/O2"]:
+            inner(flag)
 
     @unittest.skipUnless(not DISABLED, "WIP test cases")
     def test_ex_legacy_switch_002(self):
-        with cd(examples_path / "ex_legacy_switch.002"):
-            self.assertTrue(compile("main.c"))
-            self.assertTrue(disassemble("main.exe", False, extension="asm", extra_args=["-F"]))
-            rename_symbols("main.exe.asm")
-            self.assertTrue(reassemble("main.exe.asm"))
-            for char in string.ascii_uppercase:
-                self.assertTrue(diff_output(["main.exe", char], ["out.exe", char]))
-            cleanup()
+        def inner(*flags):
+            with cd(examples_path / "ex_legacy_switch.002"):
+                self.assertTrue(compile(*flags, "main.c"))
+                self.assertTrue(disassemble("main.exe", False, extension="asm", extra_args=["-F"]))
+                rename_symbols("main.exe.asm")
+                self.assertTrue(reassemble("main.exe.asm"))
+                for char in string.ascii_uppercase:
+                    self.assertTrue(diff_output(["main.exe", char], ["out.exe", char]))
+                cleanup()
+        for flag in ["/Od", "/Ot", "/O1", "/Ox", "/O2"]:
+            inner(flag)
 
     @unittest.skipUnless(not DISABLED, "WIP test cases")
     def test_ex_legacy_switch_003(self):
-        with cd(examples_path / "ex_legacy_switch.003"):
-            self.assertTrue(compile("main.c"))
-            self.assertTrue(disassemble("main.exe", False, extension="asm", extra_args=["-F"]))
-            rename_symbols("main.exe.asm")
-            self.assertTrue(reassemble("main.exe.asm"))
-            for arg in [750, 800, 900, 700, 500, 250, 100, 200, 600]:
-                self.assertTrue(diff_output(["main.exe", str(arg)], ["out.exe", str(arg)]))
-            cleanup()
+        def inner(*flags):
+            with cd(examples_path / "ex_legacy_switch.003"):
+                self.assertTrue(compile(*flags, "main.c"))
+                self.assertTrue(disassemble("main.exe", False, extension="asm", extra_args=["-F"]))
+                rename_symbols("main.exe.asm")
+                self.assertTrue(reassemble("main.exe.asm"))
+                for arg in [750, 800, 900, 700, 500, 250, 100, 200, 600]:
+                    self.assertTrue(diff_output(["main.exe", str(arg)], ["out.exe", str(arg)]))
+                cleanup()
+        for flag in ["/Od", "/Ot", "/O1", "/Ox", "/O2"]:
+            inner(flag)
 
     @unittest.skipUnless(not DISABLED, "WIP test cases")
     def test_ex_legacy_switch_004(self):
-        with cd(examples_path / "ex_legacy_switch.004"):
-            self.assertTrue(compile("main.c"))
-            self.assertTrue(disassemble("main.exe", False, extension="asm", extra_args=["-F"]))
-            rename_symbols("main.exe.asm")
-            self.assertTrue(reassemble("main.exe.asm"))
-            seq = ['002', '003', '004', '005', '006', '007', '008', '009',
-            '010', '011', '012', '013', '014', '015', '016', '017', '018',
-            '019', '020', '902', '903', '904', '905', '906', '907', '908',
-            '909', '910', '911', '912', '913', '914', '915', '916', '917',
-            '918', '919', '920', '602', '603', '604', '605', '606', '607',
-            '608', '609', '610', '611', '612', '613', '614', '615', '616',
-            '617', '618', '619', '620']
-            for arg in seq:
-                self.assertTrue(diff_output(["main.exe", str(arg)], ["out.exe", str(arg)]))
-            cleanup()
+        def inner(*flags):
+            with cd(examples_path / "ex_legacy_switch.004"):
+                self.assertTrue(compile(*flags, "main.c"))
+                self.assertTrue(disassemble("main.exe", False, extension="asm", extra_args=["-F"]))
+                rename_symbols("main.exe.asm")
+                self.assertTrue(reassemble("main.exe.asm"))
+                seq = ['002', '003', '004', '005', '006', '007', '008', '009',
+                '010', '011', '012', '013', '014', '015', '016', '017', '018',
+                '019', '020', '902', '903', '904', '905', '906', '907', '908',
+                '909', '910', '911', '912', '913', '914', '915', '916', '917',
+                '918', '919', '920', '602', '603', '604', '605', '606', '607',
+                '608', '609', '610', '611', '612', '613', '614', '615', '616',
+                '617', '618', '619', '620']
+                for arg in seq:
+                    self.assertTrue(diff_output(["main.exe", str(arg)], ["out.exe", str(arg)]))
+                cleanup()
+        for flag in ["/Od", "/Ot", "/O1", "/Ox", "/O2"]:
+            inner(flag)
 
     # TODO: Fatal error compiling CPP with CL under WINE with the current ENV.
     #       Works manually on binary compiled on Windows.
@@ -170,7 +187,7 @@ class TestPE32(unittest.TestCase):
 
     @unittest.skipUnless(not DISABLED, "WIP test cases")
     def test_ex_virtualDispatch(self):
-        self.bdrt("ex_virtualDispatch", "ex.c")
+        self.bdrt("ex_virtualDispatch", "ex.cpp")
 
 if __name__ == "__main__":
     unittest.main()
