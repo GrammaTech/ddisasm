f57a8d977cb5a31091a2f8fe0d3032a645462d93
Kevin Warrick <kwarrick@grammatech.com>
2021-02-22 12:30:33 -0500

    Add comments to wrapper script

--- examples/make.bat
+++ examples/make.bat
@@ -1,4 +1,8 @@
 @ECHO OFF
+REM NMake wrapper that initializes the required x86 or x64 VS build environment.
+REM Usage:  SET TARGET_ARCH=x86
+REM         ..\makes.bat
+REM         ..\makes.bat clean
 
 IF NOT DEFINED TARGET_ARCH (
     SET TARGET_ARCH=x64
