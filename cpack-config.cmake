e407926d8094ab6f897771d3320b2e16686156e4
Brian Fairservice <bfairservice@grammatech.com>
2021-04-21 08:29:32 -0400

    Fix references to CPACK_DEBIAN_PACKAGE_RELEASE

--- cpack-config.cmake
+++ cpack-config.cmake
@@ -5,18 +5,19 @@ if("${CPACK_DDISASM_DEBIAN_PACKAGE}" STREQUAL "ddisasm")
   set(CPACK_DEBIAN_PACKAGE_NAME "ddisasm")
   set(CPACK_PACKAGE_FILE_NAME "ddisasm")
   set(CPACK_COMPONENTS_ALL ddisasm)
-  if("${CPACK_DEBIAN_RELEASE}" STREQUAL "focal")
+  if("${CPACK_DEBIAN_PACKAGE_RELEASE}" STREQUAL "focal")
     set(CPACK_DEBIAN_PACKAGE_DEPENDS
-        "libstdc++6, libc6, libgcc1, libgomp1, libgtirb (=${CPACK_GTIRB_VERSION}-${CPACK_DEBIAN_RELEASE}), libgtirb-pprinter (=${CPACK_GTIRB_PPRINTER_VERSION}), libboost-filesystem1.71.0, libboost-program-options1.71.0, libcapstone-dev (=1:4.0.1-gt3)"
+        "libstdc++6, libc6, libgcc1, libgomp1, libgtirb (=${CPACK_GTIRB_VERSION}-${CPACK_DEBIAN_PACKAGE_RELEASE}), libgtirb-pprinter (=${CPACK_GTIRB_PPRINTER_VERSION}-${CPACK_DEBIAN_PACKAGE_RELEASE}), libboost-filesystem1.71.0, libboost-program-options1.71.0, libcapstone-dev (=1:4.0.1-gt3)"
     )
   else()
     set(CPACK_DEBIAN_PACKAGE_DEPENDS
-        "libstdc++6, libc6, libgcc1, libgomp1, libgtirb (=${CPACK_GTIRB_VERSION}-${CPACK_DEBIAN_RELEASE}), libgtirb-pprinter (=${CPACK_GTIRB_PPRINTER_VERSION}), libboost (>=1.67) | libboost1.67, libcapstone-dev (=1:4.0.1-gt3)"
+        "libstdc++6, libc6, libgcc1, libgomp1, libgtirb (=${CPACK_GTIRB_VERSION}-${CPACK_DEBIAN_PACKAGE_RELEASE}), libgtirb-pprinter (=${CPACK_GTIRB_PPRINTER_VERSION}-${CPACK_DEBIAN_PACKAGE_RELEASE}), libboost (>=1.67) | libboost1.67, libcapstone-dev (=1:4.0.1-gt3)"
     )
   endif()
 elseif("${CPACK_DDISASM_DEBIAN_PACKAGE}" STREQUAL "debug")
   set(CPACK_DEBIAN_PACKAGE_NAME "ddisasm-dbg")
   set(CPACK_PACKAGE_FILE_NAME "ddisasm-dbg")
   set(CPACK_COMPONENTS_ALL debug-file)
-  set(CPACK_DEBIAN_PACKAGE_DEPENDS "ddisasm (=${CPACK_DDISASM_VERSION})")
+  set(CPACK_DEBIAN_PACKAGE_DEPENDS
+      "ddisasm (=${CPACK_DDISASM_VERSION}-${CPACK_DEBIAN_PACKAGE_RELEASE})")
 endif()
