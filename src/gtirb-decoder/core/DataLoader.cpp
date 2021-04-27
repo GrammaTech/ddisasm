a4aa4e1e4811ef12dd6312110489ca1320f0c02a
Eric Schulte <eschulte@grammatech.com>
2021-04-27 11:15:15 -0400

    Define byteswap macros for APPLE

--- src/gtirb-decoder/core/DataLoader.cpp
+++ src/gtirb-decoder/core/DataLoader.cpp
@@ -24,12 +24,27 @@
 
 #ifdef _WIN32
 #include <windows.h>
+#endif // _WIN32
+
+#if defined(_WIN32) || defined(__APPLE__)
+#if defined(_MSC_VER)
+
 #define be32toh(x) _byteswap_ulong(x)
 #define le32toh(x) (x)
 
 #define be64toh(x) _byteswap_uint64(x)
 #define le64toh(x) (x)
-#endif // _WIN32
+
+#elif defined(__GNUC__) || defined(__clang__)
+
+#define be32toh(x) __builtin_bswap32(x)
+#define le32toh(x) (x)
+
+#define be64toh(x) __builtin_bswap64(x)
+#define le64toh(x) (x)
+
+#endif // _MSC_VER
+#endif // defined(_WIN32) || defined(__APPLE__)
 
 #include "../../AuxDataSchema.h"
 
