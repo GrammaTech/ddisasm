e4d12c4cf15ccf2c77186d407fbf9bf14c80ebfb
Brian Fairservice <bfairservice@grammatech.com>
2021-06-03 08:16:56 -0400

    Update capstone version in debian package

--- cpack-config.cmake
+++ cpack-config.cmake
@@ -8,11 +8,11 @@ if("${CPACK_DDISASM_PACKAGE}" STREQUAL "deb-ddisasm")
   set(CPACK_COMPONENTS_ALL ddisasm)
   if("${CPACK_DEBIAN_PACKAGE_RELEASE}" STREQUAL "focal")
     set(CPACK_DEBIAN_PACKAGE_DEPENDS
-        "libstdc++6, libc6, libgcc1, libgomp1, libgtirb (=${CPACK_GTIRB_VERSION}-${CPACK_DEBIAN_PACKAGE_RELEASE}), libgtirb-pprinter (=${CPACK_GTIRB_PPRINTER_VERSION}-${CPACK_DEBIAN_PACKAGE_RELEASE}), libboost-filesystem1.71.0, libboost-program-options1.71.0, libcapstone-dev (=1:4.0.1-gt3)"
+        "libstdc++6, libc6, libgcc1, libgomp1, libgtirb (=${CPACK_GTIRB_VERSION}-${CPACK_DEBIAN_PACKAGE_RELEASE}), libgtirb-pprinter (=${CPACK_GTIRB_PPRINTER_VERSION}-${CPACK_DEBIAN_PACKAGE_RELEASE}), libboost-filesystem1.71.0, libboost-program-options1.71.0, libcapstone-dev (=1:4.0.1-gt4)"
     )
   else()
     set(CPACK_DEBIAN_PACKAGE_DEPENDS
-        "libstdc++6, libc6, libgcc1, libgomp1, libgtirb (=${CPACK_GTIRB_VERSION}-${CPACK_DEBIAN_PACKAGE_RELEASE}), libgtirb-pprinter (=${CPACK_GTIRB_PPRINTER_VERSION}-${CPACK_DEBIAN_PACKAGE_RELEASE}), libboost (>=1.67) | libboost1.67, libcapstone-dev (=1:4.0.1-gt3)"
+        "libstdc++6, libc6, libgcc1, libgomp1, libgtirb (=${CPACK_GTIRB_VERSION}-${CPACK_DEBIAN_PACKAGE_RELEASE}), libgtirb-pprinter (=${CPACK_GTIRB_PPRINTER_VERSION}-${CPACK_DEBIAN_PACKAGE_RELEASE}), libboost (>=1.67) | libboost1.67, libcapstone-dev (=1:4.0.1-gt4)"
     )
   endif()
 elseif("${CPACK_DDISASM_PACKAGE}" STREQUAL "deb-debug")
