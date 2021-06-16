5dbb9d422270719859b302a48bca644d786cf535
Junghee Lim <junghee@grammatech.com>
2021-06-16 13:20:18 -0400

    Add a unit test for the issue about moved_label

--- tests/misc_test.py
+++ tests/misc_test.py
@@ -1,4 +1,6 @@
 import platform
+import os
+import subprocess
 import unittest
 from disassemble_reassemble_check import compile, cd, disassemble
 from pathlib import Path
@@ -97,6 +99,47 @@ class AuxDataTests(unittest.TestCase):
                     ]
                     break
             assert found
+
+
+class MovedLabelTests(unittest.TestCase):
+    @unittest.skipUnless(
+        platform.system() == "Linux", "This test is linux only."
+    )
+    def test_moved_label(self):
+        """
+        Test that labels are correctly moved.
+        """
+
+        binary = "ex"
+        with cd(ex_asm_dir / "ex_moved_label"):
+            self.assertTrue(compile("gcc", "g++", "-Os", []))
+            self.assertTrue(
+                disassemble(
+                    binary,
+                    "strip",
+                    False,
+                    False,
+                    format="--ir",
+                    extension="gtirb",
+                )
+            )
+
+            ir_library = gtirb.IR.load_protobuf(binary + ".gtirb")
+            m = ir_library.modules[0]
+
+            symbol_of_interest = [
+                s for s in m.symbols if s.name == "point.1"
+            ][0]
+            assert isinstance(symbol_of_interest.referent, gtirb.CodeBlock)
+
+            block = symbol_of_interest.referent
+            bi = block.byte_interval
+            sexpr = set(bi.symbolic_expressions_at(range(block.address,block.address+block.size)))
+            self.assertEqual(len(sexpr), 1)
+            se1 = next(iter(sexpr))[2]
+            assert isinstance(se1, gtirb.SymAddrConst)
+            self.assertEqual(se1.symbol.name, "point.2")
+            self.assertEqual(se1.offset, 22)
 
 
 if __name__ == "__main__":
