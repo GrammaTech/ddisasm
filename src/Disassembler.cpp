481310a8bded1f91f84491ec61c6e3ac4c19f8cf
Joshua Robbins <jrobbins@grammatech.com>
2021-08-09 12:16:15 -0400

    Fix errors from changes to GTIRB's section ranges

--- src/Disassembler.cpp
+++ src/Disassembler.cpp
@@ -896,24 +896,25 @@ void buildBSS(gtirb::Context &context, gtirb::Module &module, souffle::SoufflePr
     {
         std::string sectionName;
         output >> sectionName;
-        const auto bss_section = module.findSections(sectionName);
-        if(bss_section == module.sections_by_name_end())
-            continue;
-        // for each bss section we divide in data objects according to the bss_data markers that
-        // fall within the range of the section
-        auto beginning = bssData.lower_bound(bss_section->getAddress().value());
-        // end points to the address at the end of the bss section
-        auto end = bssData.lower_bound(*addressLimit(*bss_section));
-        for(auto i = beginning; i != end; ++i)
+        const auto bss_sections = module.findSections(sectionName);
+        for(const auto &bss_section : bss_sections)
         {
-            auto next = i;
-            next++;
-            if(auto it = module.findByteIntervalsOn(*i); !it.empty())
+            // for each bss section we divide in data objects according to the bss_data markers that
+            // fall within the range of the section
+            auto beginning = bssData.lower_bound(bss_section.getAddress().value());
+            // end points to the address at the end of the bss section
+            auto end = bssData.lower_bound(*addressLimit(bss_section));
+            for(auto i = beginning; i != end; ++i)
             {
-                gtirb::ByteInterval &byteInterval = *it.begin();
-                uint64_t blockOffset = *i - byteInterval.getAddress().value();
-                byteInterval.addBlock<gtirb::DataBlock>(context, blockOffset,
-                                                        static_cast<uint64_t>(*next - *i));
+                auto next = i;
+                next++;
+                if(auto it = module.findByteIntervalsOn(*i); !it.empty())
+                {
+                    gtirb::ByteInterval &byteInterval = *it.begin();
+                    uint64_t blockOffset = *i - byteInterval.getAddress().value();
+                    byteInterval.addBlock<gtirb::DataBlock>(context, blockOffset,
+                                                            static_cast<uint64_t>(*next - *i));
+                }
             }
         }
     }
