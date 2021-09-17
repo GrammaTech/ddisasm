d5f8aedadb2c99d67e78ff9cac5bf66a903defee
Antonio Flores Montoya <afloresmontoya@grammatech.com>
2021-09-17 09:03:17 -0400

    re-add mcpp path to conan and adapt conan-ubuntu18

--- conanfile.py
+++ conanfile.py
@@ -171,11 +171,12 @@ class DdisasmConan(Properties, ConanFile):
         else:
             cmake = CMake(self, generator=None)
             defs.update({"GTIRB_PPRINTER_STRIP_DEBUG_SYMBOLS:BOOL": "ON"})
+            self.add_dep_bin_path("mcpp")
 
         if self.settings.build_type == "Release":
             cmake.build_type = "RelWithDebInfo"
         self.add_dep_bin_path("gtirb-pprinter")
-        self.add_dep_lib_path("gtirb-pprinter", "gtirb", "capstone", "libffi")
+        self.add_dep_lib_path("gtirb-pprinter", "gtirb", "capstone")
         bin_dir = os.path.join(os.getcwd(), "bin")
         os.environ["PATH"] = os.pathsep.join([os.environ["PATH"], bin_dir])
 
