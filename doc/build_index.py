137f895f81164e2f81d72772ff3bbe32af187c7b
Antonio Flores Montoya <afloresmontoya@grammatech.com>
2021-09-24 16:38:25 -0400

    no need to generate the index, we can use glob there

--- doc/build_index.py
+++ doc/build_index.py
@@ -4,35 +4,26 @@ from pathlib import Path
 
 DDISASM_ROOT = Path(__file__).resolve().parent.parent
 
-index_prefix = """
-Source files
-=============================
 
-.. toctree::
-    :maxdepth: 2
-
-"""
-index_suffix = """
-:ref:`predicateindex`
-"""
-
-
-def build_main_index():
-    index_text = index_prefix
+def build_main_index() -> None:
+    """
+    Build the documentation files
+    corresponding to each of the source files
+    """
     dl_docs = DDISASM_ROOT / "doc" / "src_docs"
     for dl_file in sorted(
         glob.glob(f"{DDISASM_ROOT}/src/datalog/**/*.dl", recursive=True)
     ):
         dl_file = dl_file[len(f"{DDISASM_ROOT}/src/datalog/") : -len(".dl")]
-        print(f"adding {dl_file} to index")
-        (dl_docs / dl_file).with_suffix(".rst").write_text(
+        print(f"creating {dl_file} in /doc/src_docs/")
+        source_doc_page = (dl_docs / dl_file).with_suffix(".rst")
+        if not source_doc_page.parent.exists():
+            source_doc_page.parent.mkdir()
+        source_doc_page.write_text(
             f"{dl_file}\n"
             "==========================================\n\n"
             f".. dl:autofile:: {dl_file}.dl\n\n"
         )
-        index_text += f"    src_docs/{dl_file}.rst\n"
-    index_text += index_suffix
-    (DDISASM_ROOT / "doc" / "index.rst").write_text(index_text)
 
 
 if __name__ == "__main__":
