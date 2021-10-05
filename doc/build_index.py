10c0d0972936d74be1e7d45936247a64d4171d2e
Antonio Flores Montoya <afloresmontoya@grammatech.com>
2021-10-04 22:35:26 -0400

    add predicate dependencies using souffle

--- doc/build_index.py
+++ doc/build_index.py
@@ -1,8 +1,16 @@
+from collections import defaultdict
 import glob
-
+import pydot
 from pathlib import Path
+import subprocess
+
 
 DDISASM_ROOT = Path(__file__).resolve().parent.parent
+
+DL_DOCS = DDISASM_ROOT / "doc" / "src_docs"
+
+
+ARCHITECTURES = ["ARCH_ARM64", "ARCH_IA32", "ARCH_AMD64"]
 
 
 def build_main_index() -> None:
@@ -10,13 +18,12 @@ def build_main_index() -> None:
     Build the documentation files
     corresponding to each of the source files
     """
-    dl_docs = DDISASM_ROOT / "doc" / "src_docs"
     for dl_file in sorted(
         glob.glob(f"{DDISASM_ROOT}/src/datalog/**/*.dl", recursive=True)
     ):
         dl_file = dl_file[len(f"{DDISASM_ROOT}/src/datalog/") : -len(".dl")]
         print(f"creating {dl_file} in /doc/src_docs/")
-        source_doc_page = (dl_docs / dl_file).with_suffix(".rst")
+        source_doc_page = (DL_DOCS / dl_file).with_suffix(".rst")
         if not source_doc_page.parent.exists():
             source_doc_page.parent.mkdir()
         source_doc_page.write_text(
@@ -26,5 +33,34 @@ def build_main_index() -> None:
         )
 
 
+def build_dependecy_graph() -> None:
+    """
+    Build the datalog dependency graph by calling souffle with each of the
+    defined architectures. Dependencies are stored in `dependencies.csv`.
+    """
+
+    dependencies = defaultdict(set)
+    for arch in ARCHITECTURES:
+        print(f"computing dependency graph for {arch}")
+        dot_text = subprocess.check_output(
+            [
+                "souffle",
+                f"{DDISASM_ROOT}/src/datalog/main.dl",
+                f"-M{arch}",
+                "--show=precedence-graph-text",
+            ]
+        )
+        g: pydot.Graph = pydot.graph_from_dot_data(dot_text.decode("utf8"))[0]
+        for edge in g.get_edges():
+            dependencies[edge.get_source().replace('"', "")].add(
+                edge.get_destination().replace('"', "")
+            )
+    with open(DL_DOCS / "dependencies.csv", mode="w") as f:
+        for src in sorted(dependencies):
+            for dest in sorted(dependencies[src]):
+                print(src, dest, file=f)
+
+
 if __name__ == "__main__":
     build_main_index()
+    build_dependecy_graph()
