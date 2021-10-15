e765ba696e7e686247801c13447e73d0a6e234f3
Kevin Warrick <kwarrick@grammatech.com>
2021-10-14 23:59:11 -0400

    Move macros

---
+++ src/gtirb-decoder/Endian.h
@@ -0,0 +1,34 @@
+#ifndef SRC_GTIRB_DECODER_ENDIAN_H_
+#define SRC_GTIRB_DECODER_ENDIAN_H_
+
+#ifdef _WIN32
+#include <windows.h>
+#endif // _WIN32
+
+#if defined(_WIN32) || defined(__APPLE__)
+#if defined(_MSC_VER)
+
+#define be16toh(x) _byteswap_uint16(x)
+#define le16toh(x) (x)
+
+#define be32toh(x) _byteswap_ulong(x)
+#define le32toh(x) (x)
+
+#define be64toh(x) _byteswap_uint64(x)
+#define le64toh(x) (x)
+
+#elif defined(__GNUC__) || defined(__clang__)
+
+#define be16toh(x) __builtin_bswap16(x)
+#define le16toh(x) (x)
+
+#define be32toh(x) __builtin_bswap32(x)
+#define le32toh(x) (x)
+
+#define be64toh(x) __builtin_bswap64(x)
+#define le64toh(x) (x)
+
+#endif // _MSC_VER
+#endif // defined(_WIN32) || defined(__APPLE__)
+
+#endif // SRC_GTIRB_DECODER_ENDIAN_H_
--- src/gtirb-decoder/core/DataLoader.cpp
+++ src/gtirb-decoder/core/DataLoader.cpp
@@ -21,32 +21,8 @@
 //
 //===----------------------------------------------------------------------===//
 #include "DataLoader.h"
-
-#ifdef _WIN32
-#include <windows.h>
-#endif // _WIN32
-
-#if defined(_WIN32) || defined(__APPLE__)
-#if defined(_MSC_VER)
-
-#define be32toh(x) _byteswap_ulong(x)
-#define le32toh(x) (x)
-
-#define be64toh(x) _byteswap_uint64(x)
-#define le64toh(x) (x)
-
-#elif defined(__GNUC__) || defined(__clang__)
-
-#define be32toh(x) __builtin_bswap32(x)
-#define le32toh(x) (x)
-
-#define be64toh(x) __builtin_bswap64(x)
-#define le64toh(x) (x)
-
-#endif // _MSC_VER
-#endif // defined(_WIN32) || defined(__APPLE__)
-
 #include "../../AuxDataSchema.h"
+#include "../Endian.h"
 #include "../UTF16.h"
 #include "../UTF8.h"
 
