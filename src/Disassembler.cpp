1344042f0267beac52e5ca7f41d8847266e2e1c5
Kevin Warrick <kwarrick@grammatech.com>
2021-12-14 16:33:34 -0500

    Replace GOT-offset sym-sym

--- src/Disassembler.cpp
+++ src/Disassembler.cpp
@@ -486,6 +486,11 @@ void buildSymbolForwarding(gtirb::Context &Context, gtirb::Module &Module,
             SymbolForwarding[Symbol->getUUID()] = NewSymbol->getUUID();
         }
     }
+    if(const auto &It = Module.findSymbols("_GLOBAL_OFFSET_TABLE_"); !It.empty())
+    {
+        gtirb::Symbol *Symbol = &*It.begin();
+        SymbolForwarding[Symbol->getUUID()] = Symbol->getUUID();
+    }
     Module.addAuxData<gtirb::schema::SymbolForwarding>(std::move(SymbolForwarding));
 }
 
