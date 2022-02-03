abc9d5793523c5606287ca97ef2842d3c79711ee
Antonio Flores Montoya <afloresmontoya@grammatech.com>
2022-02-03 17:04:20 +0000

    copy all files in "bin" to create the package.

--- conanfile.py
+++ conanfile.py
@@ -193,12 +193,7 @@ class DdisasmConan(Properties, ConanFile):
                 cmake.test(output_on_failure=True)
 
     def package(self):
-        self.copy("*.h", dst="include", src=self.name)
-        self.copy("*%s.lib" % (self.name), dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        self.copy("*", src="bin", dst="bin", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = [self.name]
