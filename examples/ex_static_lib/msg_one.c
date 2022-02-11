463028baa58fd161cfdd722ef191dc06c98b9c36
Adam Seitz <aseitz@grammatech.com>
2022-02-11 12:05:00 -0500

    Fix x86-32 no-pie, section-relative relocations
    * Handle REL implicit addends
    * handle SECTION-relative relations with no symbol in the middle of the
      section

--- examples/ex_static_lib/msg_one.c
+++ examples/ex_static_lib/msg_one.c
@@ -4,3 +4,10 @@ char *get_msg_one(void)
 {
     return "one";
 }
+
+// Having a second function implemented here (with a second string) ensures
+// there is a string in the .rodata section that is *not* at the section start.
+char *get_msg_five(void)
+{
+    return "five";
+}
