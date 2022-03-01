f0e9cbc09302326e41345b1628f284a038021d02
Kevin Warrick <kwarrick@grammatech.com>
2022-03-01 13:41:15 -0500

    Run tests in new vcvars env

--- conanfile.py
+++ conanfile.py
@@ -189,6 +189,9 @@ class DdisasmConan(Properties, ConanFile):
         if self.settings.build_type == "Release":
             with tools.vcvars(self.settings, arch="x86"):
                 cmake.test(output_on_failure=True)
+            # FIXME: https://github.com/conan-io/conan/issues/3673
+            # Remove environment variable to force vcvars configuration.
+            os.environ.pop("VisualStudioVersion", None)
             with tools.vcvars(self.settings, arch="x86_64"):
                 cmake.test(output_on_failure=True)
 
