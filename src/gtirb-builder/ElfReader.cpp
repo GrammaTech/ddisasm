c34911bac64977368cc42d61ab2844d9c97c2517
Adam Seitz <aseitz@grammatech.com>
2022-03-04 16:49:27 -0500

    Skip relocations applied to unloaded sections

--- src/gtirb-builder/ElfReader.cpp
+++ src/gtirb-builder/ElfReader.cpp
@@ -714,6 +714,17 @@ void ElfReader::addAuxData()
     {
         std::string SymbolName;
         std::string SectionName;
+        if(Relocation.has_section())
+        {
+            LIEF::ELF::Section &Section = Relocation.section();
+            if(!Section.has(LIEF::ELF::ELF_SECTION_FLAGS::SHF_ALLOC))
+            {
+                // Ignore relocations that are applied to un-loaded sections.
+                continue;
+            }
+            SectionName = Section.name();
+        }
+
         if(Relocation.has_symbol())
         {
             SymbolName = Relocation.symbol().name();
@@ -725,12 +736,6 @@ void ElfReader::addAuxData()
                 {
                     SymbolName.append("@" + SymbolVersion.symbol_version_auxiliary().name());
                 }
-            }
-
-            if(Relocation.has_section())
-            {
-                LIEF::ELF::Section &Section = Relocation.section();
-                SectionName = Section.name();
             }
         }
 
