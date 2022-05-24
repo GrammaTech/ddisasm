c54a903b66947dfe8c3c3e9cd8eb245d3e6a2421
Adam Seitz <aseitz@grammatech.com>
2022-05-24 14:52:20 -0400

    Fix conan build errors

--- conanfile.py
+++ conanfile.py
@@ -39,7 +39,7 @@ class Properties:
     name = "ddisasm"
     version = get_version()
     rel_url = "rewriting/ddisasm"
-    exports_sources = "*"
+    exports_sources = "*", "!.conan/*"
 
     @property
     def description(self):
