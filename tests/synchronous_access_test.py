48cf3fb3cc739b2a20db9d265b858fb426d121f0
Junghee Lim <junghee@grammatech.com>
2022-12-15 09:00:19 -0500

    Review change

--- tests/synchronous_access_test.py
+++ tests/synchronous_access_test.py
@@ -18,20 +18,25 @@ class SynchronousAccessTests(unittest.TestCase):
         Test that the bss section does not get split by
         a synchronous access.
         """
+        for exdir in (ex_dir, ex_asm_dir):
+            with self.subTest(example=exdir / "ex_synchronous_access"):
+                binary = "ex"
+                with cd(exdir / "ex_synchronous_access"):
+                    self.assertTrue(compile("gcc", "g++", "-O0", []))
+                    self.assertTrue(
+                        disassemble(binary, format="--ir", strip=True)[0]
+                    )
 
-        binary = "ex"
-        with cd(ex_asm_dir / "ex_synchronous_access"):
-            self.assertTrue(compile("gcc", "g++", "-O0", []))
-            self.assertTrue(disassemble(binary, format="--ir", strip=True)[0])
+                    ir_library = gtirb.IR.load_protobuf(binary + ".gtirb")
+                    m = ir_library.modules[0]
 
-            ir_library = gtirb.IR.load_protobuf(binary + ".gtirb")
-            m = ir_library.modules[0]
-
-            bss_section = next(s for s in m.sections if s.name == ".bss")
-            # There is a data block in .bss of size 80
-            self.assertIn(
-                80, [block.size for block in bss_section.data_blocks]
-            )
+                    bss_section = next(
+                        s for s in m.sections if s.name == ".bss"
+                    )
+                    # There is a data block in .bss of size 80
+                    self.assertIn(
+                        80, [block.size for block in bss_section.data_blocks]
+                    )
 
     @unittest.skipUnless(
         platform.system() == "Linux", "This test is linux only."
@@ -75,29 +80,6 @@ class SynchronousAccessTests(unittest.TestCase):
             ]
             # Three synchronized accesses propagated in a loop with 10 elements
             self.assertGreaterEqual(len(addresses_in_data), 30)
-
-    @unittest.skipUnless(
-        platform.system() == "Linux", "This test is linux only."
-    )
-    def test_synchronous_access_in_bss2(self):
-        """
-        Test that the bss section does not get split by
-        a synchronous access.
-        """
-
-        binary = "ex"
-        with cd(ex_dir / "ex_synchronous_access"):
-            self.assertTrue(compile("gcc", "g++", "-O0", []))
-            self.assertTrue(disassemble(binary, format="--ir", strip=True)[0])
-
-            ir_library = gtirb.IR.load_protobuf(binary + ".gtirb")
-            m = ir_library.modules[0]
-
-            bss_section = next(s for s in m.sections if s.name == ".bss")
-            # There is a data block in .bss of size 80
-            self.assertIn(
-                80, [block.size for block in bss_section.data_blocks]
-            )
 
     @unittest.skipUnless(
         platform.system() == "Linux", "This test is linux only."
