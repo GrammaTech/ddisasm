8ccba59bafed78094138ad5054f5aa167df15359
Junghee Lim <junghee@grammatech.com>
2023-02-20 09:39:55 -0500

    Extend the block_point for "dangling block with enclosed instr"
    And augment the unit test.

--- examples/asm_examples/ex_enclosed_instr/ex_original.s
+++ examples/asm_examples/ex_enclosed_instr/ex_original.s
@@ -7,6 +7,12 @@
     .quad  .L1+3
 .LC1:
     .quad  .L0
+    .zero  7
+    .quad  .L7
+    .zero  7
+    .quad  .L7+3
+    .zero  7
+    .quad  .L4+1
 
     .text
 
@@ -46,6 +52,34 @@ bar:
     jmp     *.LC1(%rip)
 .L3:
     movl    $0, %eax
+    popq    %rbp
+    ret
+
+    .type   baz, @notype
+baz:
+    pushq   %rbp
+    movq    %rsp, %rbp
+    subq    $1024, %rsp
+    jmp     .L5
+    ja      .L5
+    nop
+    nop
+    nop
+.L7:
+    xor     %rax, %rax
+.L4:
+    # At .L4+1, there is an instruction `XOR` (size 2) and
+    # at .L4+3, `ADD` (size 2), both of which are enclosed by the `MOV`
+    # (size 5) instruction in block .L4.
+    # This example is to demonstrate that .L4 wins over the block starting
+    # at .L4+1 by block_point("dangling block with enclosed instr").
+    movl    $49, %edi
+    jmp     .L6
+.L5:
+    jmp     *.LC1(%rip)
+.L6:
+    movl    $0, %eax
+    addq    $1024, %rsp
     popq    %rbp
     ret
 
